ecc4f6b2defe7159bfbd29574e32da5e8b5133fd
Kristian Nedrevold <nedrevold.kristian@gmail.com>
2021-04-08 00:28:25 +0200

    blog

---
+++ bruker.py
@@ -0,0 +1,10 @@
+class Bruker():
+    """ Egenskapene til en bruker av webapplikasjonen """
+
+    def __init__(self, brukernavn, epost, opprettet, admin):
+        self.brukernavn = brukernavn
+        self.epost = epost
+        self.opprettet = opprettet
+        self.admin = admin
+
+
--- db.py
+++ db.py
@@ -10,3 +10,28 @@ password = config['mysqlDB']['pass']
 db = config['mysqlDB']['db']
 
 
+class OppslagReg:
+
+    def __init__(self) -> None:
+        dbconfig = {'host': host,
+                    'user': user,
+                    'password': password,
+                    'database': db, }
+        self.configuration = dbconfig
+
+    def __enter__(self) -> 'cursor':
+        self.conn = mysql.connector.connect(**self.configuration)
+        self.cursor = self.conn.cursor(prepared=True)
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_trace) -> None:
+        self.conn.commit()
+        self.cursor.close()
+        self.conn.close()
+
+    def query(self, sql):
+        self.cursor.execute(sql)
+        result = self.cursor.fetchall()
+        return result
+
+
